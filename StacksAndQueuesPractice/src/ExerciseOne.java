bb2ee5f02adb8231218ea879323248e65108d3d1
Roy Varghese Mathew <RoyM77@gmail.com>
2015-07-27 11:14:36 -0700

    1st Attempt

--- StacksAndQueuesPractice/src/ExerciseOne.java
+++ StacksAndQueuesPractice/src/ExerciseOne.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class ExerciseOne {
 
@@ -5,5 +8,35 @@ public class ExerciseOne {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	public static Stack<Integer> splitStack(Stack<Integer> toSplit) {
+		Queue<Integer> aux = new LinkedList();
+		
+		// Transfer values from stack to Queue
+		while (!toSplit.isEmpty()){
+			aux.add(toSplit.pop());
+		}
+		
+		int auxSize = aux.size();
+		
+		// Places negative values in stack
+		for (int i = 0; i < auxSize; i++){
+			Integer toSort = aux.remove();
+			if (toSort.intValue() < 0){
+				toSplit.add(toSort);
+			}else {
+				aux.add(toSort);
+			}
+		}
+		
+		// adds remaining non-negative values to stack
+		while (!aux.isEmpty()){
+			toSplit.add(aux.remove());
+		}
+		
+		// Return split Stack
+		return toSplit;
+		
+	}
 }
+
