1dc4f452120133c868e3d0aa786144126555eadf
Roy Varghese Mathew <RoyM77@gmail.com>
2015-10-07 10:45:40 -0700

    2nd commit

--- Reddit Challenge 234/src/Solution.java
+++ Reddit Challenge 234/src/Solution.java
@@ -1,29 +1,52 @@
 // https://www.reddit.com/r/dailyprogrammer/comments/3n55f3/20150930_challenge_234_intermediate_red_squiggles/
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Solution {
 
 	public static void main(String[] args) {
 		File f = new File("enable1.txt");
-		Scanner file = null;
-		String word = "Hello";
+		Scanner words = null;
+		//String word = "Hello";
 		try {
-			file = new Scanner(f);
+			words = new Scanner(f);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
-		//HashSet of words here
+		List<String> wordsList = new LinkedList<String>();
+		
+		while (words.hasNextLine()){
+			String word = words.nextLine();
+			wordsList.add(word);
+		}
 		
 		//loop through words here
-			//Set filtered Words = filterWords(String word, Set words)
+			String word = "gme";
+			List<String> filteredWords = filterWords(wordsList, "word");
 			// loop though characters here and insert necessary indicator
+			for (int i = 0; i < word.length(); i++){
+				
+			}
 		
 	}
 
-	//method create initial filtered words
+	//method creates initial filtered words set
+	private static List<String> filterWords(List<String> wordsList, String word) {
+		char wordFirstLetter = word.charAt(0);
+		List<String> filteredWords = new LinkedList<String>();
+		Iterator<String> iter = wordsList.iterator();
+		while(iter.hasNext()) {
+			String nextWord = iter.next();
+			char nextWordFirstLetter = nextWord.charAt(0);
+			if (nextWordFirstLetter == wordFirstLetter) {
+				filteredWords.add(nextWord);
+			}// else if ()
+		}
+		return filteredWords;
+	}
 }
