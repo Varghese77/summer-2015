3e8734a223aea326646f4c3b8256f9416446779a
Roy Varghese Mathew <RoyM77@gmail.com>
2015-10-08 11:38:07 -0700

    first commit

---
+++ ChemistryHelper/src/LightCalculator.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LightCalculator {
+
+	private static Map<Character, Double> metricConverter;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		metricConverter = new HashMap<Character, Double>();
+	}
+
+	private static void deBrogileWavelength(double mass, double velocity){
+		
+	}
+	
+	private static void convert(String measurment, String finalUnit){
+		measurment = measurment.trim();
+		finalUnit.trim();
+		
+	}
+	
+	private static void convertMass(double n, String initialUnit, String finalUnit) {
+		
+	}
+	
+	private static void convertUnit(double n, String unit) {
+		
+	}
+}
